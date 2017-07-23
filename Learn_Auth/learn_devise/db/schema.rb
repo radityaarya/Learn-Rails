218aa304ffb7775c65f904c530033b4f056bf513
Raditya <arya.raditya26@gmail.com>
2017-07-23 21:14:45 +0700

    Learn_Auth/learn_devise : generate post scaffold and user controller

--- Learn_Auth/learn_devise/db/schema.rb
+++ Learn_Auth/learn_devise/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723111029) do
+ActiveRecord::Schema.define(version: 20170723140448) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "username"
